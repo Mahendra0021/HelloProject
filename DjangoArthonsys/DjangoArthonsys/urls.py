1671c7eaae3a65e18e818fdf489b74f2b2ec06db
Mahendra <sainmahendra171@gmail.com>
2023-12-14 19:11:23 +0530

    import include class

--- DjangoArthonsys/DjangoArthonsys/urls.py
+++ DjangoArthonsys/DjangoArthonsys/urls.py
@@ -22,7 +22,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('',views.index,name=""),
-    # path('Contacts/', views.contact, name="Contacts"),
+        # path('Contacts/', views.contact, name="Contacts"),
     path('',include('Arthonsys.urls'))
 
 ]
