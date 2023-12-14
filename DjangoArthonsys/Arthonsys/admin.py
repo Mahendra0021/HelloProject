22da231aa037d234fa44a110d3db2e4e98c19377
Mahendra <sainmahendra171@gmail.com>
2023-12-14 19:00:24 +0530

    Register the table name in admin

--- DjangoArthonsys/Arthonsys/admin.py
+++ DjangoArthonsys/Arthonsys/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import RegisterModel
 
-# Register your models here.
+@admin.register(RegisterModel)
+class AuthorAdmin(admin.ModelAdmin):
+  list_display = ['User_name','Email','password','gender','city','skill']
